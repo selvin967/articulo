5ad21638194817612b441a7f47164644af1b3eda
selvin967 <9g8ndcskvz@privaterelay.appleid.com>
2026-06-24 10:44:46 -0600

    fix(categories/index): correction button name

--- resources/views/Category/index.blade.php
+++ resources/views/Category/index.blade.php
@@ -1,9 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <div class="flex items-center justify-between">
-            <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-                Categorías
-            </h2>
+            <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">Categorías</h2>
             <a href="{{ route('categories.create') }}" class="px-4 py-2 rounded-lg bg-blue-600 text-white hover:bg-blue-700">
                 Crear categoría
             </a>
